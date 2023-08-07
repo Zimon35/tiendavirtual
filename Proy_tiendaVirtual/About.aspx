0142306e771b7e293da78fd077b9fc3887b20769
Zimon35 <schoque@senati.pe>
2023-08-07 09:36:42 -0500

    Primer commit

---
+++ Proy_tiendaVirtual/About.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Proy_tiendaVirtual.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h2 id="title"><%: Title %>.</h2>
+        <h3>Mi Primera Aplicacion web con tecnologias ASP.</h3>
+        <p>Aplicando IA.</p>
+    </main>
+</asp:Content>
---
+++ Proy_tiendaVirtual/About.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Proy_tiendaVirtual
+{
+
+
+    public partial class About
+    {
+    }
+}
